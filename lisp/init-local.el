33942c84b690d71ae53ecf7dcb9a7059b4978ea6
ethanfu <fuxinjie0309@gmail.com>
2023-12-03 17:50:35 +0800

    去掉local自定义函数

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,7 +1,7 @@
 ;;; init-local.el --- local config -*- lexical-binding: t -*-
 
-(add-hook 'emacs-startup-hook 'toggle-frame-fullscreen)
-(define-key global-map (kbd "S-SPC") 'set-mark-command)
+;;;(add-hook 'emacs-startup-hook 'toggle-frame-fullscreen)
+;;;(define-key global-map (kbd "S-SPC") 'set-mark-command)
 
 
 (provide 'init-local)
